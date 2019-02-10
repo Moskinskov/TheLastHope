224ecbb1c00fd39095a8a50c395d442e7cd184a6
Dmitri Kuzmin <dkuzmin@brite-apps.com>
2019-02-10 20:11:29 +0300

    Clean-up attempt.

--- Assets/Scripts/ADestroyer.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using UnityEngine;
-
-
-public abstract class ADestroyer : MonoBehaviour
-{
-    public abstract void Destroy(SceneData sceneData);
-}
--- Assets/Scripts/AWorldMover.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using UnityEngine;
-
-
-public abstract class AWorldMover : MonoBehaviour
-{
-    public abstract void MoveWorld(SceneData sceneData, float deltaTime);
-}
