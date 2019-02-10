2f64a6aa01e72d2e1c738da38143f700c75c200d
Buffer112 <gogi-pankraxin@mail.ru>
2019-02-10 21:21:58 +0300

    Добавил скрипты и тесты контроллера турели

---
+++ Assets/Turrel/Scripts/BulletController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour {
+	public int speed = 50;
+	private Transform bulletItem;
+
+	// Use this for initialization
+	void Start () {
+		bulletItem = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		bulletItem.position += bulletItem.forward * speed * Time.deltaTime;
+	}
+
+	protected virtual void OnTriggerEnter(Collider col) {
+		Destroy(gameObject);
+	}
+}
