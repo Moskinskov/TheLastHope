95c60640380c745e152e56014608c03dbaacb2ea
Dmitri Kuzmin <dkuzmin@brite-apps.com>
2019-02-14 17:50:41 +0300

    Fixed Data namespace issues.

--- Assets/Scripts/Data/SceneData.cs
+++ Assets/Scripts/Data/SceneData.cs
@@ -2,24 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SceneData
+namespace TheLastHope.Data
 {
-    public List<GameObject> props;
-    public List<GameObject> enemies;
-    public List<GameObject> trainCars;
-    public List<GameObject> rails;
-    public List<GameObject> enemiesPatterns;
-    public float targetEnemyCount;
-    public float targetPropsCount;
-    public float trainSpeed;
+	public class SceneData
+	{
+		public List<GameObject> props;
+		public List<GameObject> enemies;
+		public List<GameObject> trainCars;
+		public List<GameObject> rails;
+		public List<GameObject> enemiesPatterns;
+		public float targetEnemyCount;
+		public float targetPropsCount;
+		public float trainSpeed;
 
-    public SceneData()
-    {
-        props = new List<GameObject>();
-        enemies = new List<GameObject>();
-        trainCars = new List<GameObject>();
-        rails = new List<GameObject>();
-        enemiesPatterns = new List<GameObject>();
-    }
+		public SceneData()
+		{
+			props = new List<GameObject>();
+			enemies = new List<GameObject>();
+			trainCars = new List<GameObject>();
+			rails = new List<GameObject>();
+			enemiesPatterns = new List<GameObject>();
+		}
+
+	}
 
 }
