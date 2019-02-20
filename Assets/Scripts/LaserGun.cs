057a67664854ffaaf5a56c917842461fdb3092dc
Moskinskov <moskinskov@gmail.com>
2019-02-20 23:41:18 +0300

    AEnergeticWeapon + LaserGun (производный от него)

---
+++ Assets/Scripts/LaserGun.cs
@@ -0,0 +1,44 @@
+﻿using Assets.Scripts.Abstract;
+using UnityEngine;
+
+namespace TheLastHope.Weapons
+{
+    public class LaserGun : AEnergeticWeapon
+    {
+        [Header("Необходимые компоненты"), Tooltip("Мушка, отсюда вылетает лазер")]
+        public Transform _LGMuzzle;
+        [Tooltip("Ну это понятно")]
+        public AudioSource LGAudioSource;
+        [Tooltip("Сам лазер")]
+        public LineRenderer _LGLineRenderer;
+        [Tooltip("Эффект от попадания")]
+        public GameObject _LGLaserEffect;
+
+        [Header("Урон лазера"), Space(10)]
+        [Tooltip("Ну это понятно")]
+        public float _LGDamagePerSecond;
+
+        [Header("Енергия лазера"), Space(10)]
+        [Tooltip("Общий объем энергии")]
+        public float _LGEnergyCapacity;
+        [Tooltip("Ну это понятно")]
+        public float _LGEnergyPerSecond;
+        [Tooltip("Ну это понятно")]
+        public float _LGRecoveryPerSecond;
+        [Tooltip("Минимальное кол-во энергии для начала стрельбы")]
+        public float _LGMinActiveEnergy;
+
+
+        private void Awake()
+        {
+            _energyCapacity = _LGEnergyCapacity;
+            _energyPerSecond = _LGEnergyPerSecond;
+            _damagePerSecond = _LGDamagePerSecond;
+            _recoveryPerSecond = _LGRecoveryPerSecond;
+            _muzzle = _LGMuzzle;
+            _origLR = _LGLineRenderer;
+            _minActiveEnergy = _LGMinActiveEnergy;
+            _laserEffect = _LGLaserEffect;
+        }
+    }
+}
--- Assets/Scripts/WeaponController.cs
+++ Assets/Scripts/WeaponController.cs
@@ -1,5 +1,4 @@
-﻿using TheLastHope.Data;
-using TheLastHope.Helpers;
+﻿using Assets.Scripts.Abstract;
 using UnityEngine;
 
 namespace TheLastHope.Weapons
@@ -7,13 +6,23 @@ namespace TheLastHope.Weapons
     public class WeaponController : MonoBehaviour
     {
         public ARangedWeapon _weapon;
-     
+        public AEnergeticWeapon _Eweapon;
+
+        private void Update()
+        {
+            if (Input.GetButton("Fire1"))
+            {
+                _Eweapon.Fire();
+            }
+        }
+
+
+
         public void UpdateWeapons(bool lmbIsPressed)
         {
             if (lmbIsPressed)
             {
                 _weapon.Fire();
-				
             }
             //if (Input.GetKeyDown(KeyCode.R))
             //    _weapon.Reload();
