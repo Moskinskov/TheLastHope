77d9749235aeeb569b4e333f18ac8ccce86b841e
Buffer112 <gogi-pankraxin@mail.ru>
2019-02-24 06:48:31 +0300

    Добавил файлы: Shotgun, Rifle. Поменял немного логику ARangedWeapon.  И поменял MachineGun.

--- Assets/Scripts/Rifle.cs
+++ Assets/Scripts/Rifle.cs
@@ -1,23 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TheLastHope.Weapons;
-using TheLastHope.Data;
 
 namespace TheLastHope.Weapons
-{
-	public sealed class MachineGun : ARangedWeapon
-	{
+{ 
+    public class Rifle : ARangedWeapon
+    {
         public override void Shot()
         {
             AAmmo _bullet = Instantiate(_ammo, _muzzle.position, _muzzle.rotation);
             var _bulletRigidBody = _bullet.GetComponent<Rigidbody>();
             _bulletRigidBody.AddForce(_muzzle.forward * _force);
         }
-
         public override void SwitchFiringMode()
-		{
-			throw new System.NotImplementedException();
-		}
-	}
+        {
+            throw new System.NotImplementedException();
+        }
+    }
 }
