b18c0bc7dae4134a37fe8863d1359913ae6b8466
Buffer112 <gogi-pankraxin@mail.ru>
2019-02-24 06:48:31 +0300

    Добавил файлы: Shotgun, Rifle. Поменял немного логику ARangedWeapon.  И поменял MachineGun.

--- Assets/Scripts/Abstract/ARangedWeapon.cs
+++ Assets/Scripts/Abstract/ARangedWeapon.cs
@@ -8,14 +8,13 @@ namespace TheLastHope.Weapons
 {
 	public abstract class ARangedWeapon : MonoBehaviour //change to BaseObject when it is ready
 	{
-
 		#region Serializables
 		[SerializeField] protected float _rateOfFire = 1.0f; //firing speed
 		[SerializeField] protected float _shootingRange = 30.0f; //firing range
 		[SerializeField] protected float _reloadTime = 1.0f; //reload time
-		[SerializeField] protected float _clipSize = 5.0f; //clip size
 		[SerializeField] protected float _force = 30.0f; //power of fire
-		[SerializeField] protected bool _canChangeFiringMode = false; //ability to change firing mode
+        [SerializeField] protected int _clipSize = 5; //clip size
+        [SerializeField] protected bool _canChangeFiringMode = false; //ability to change firing mode
 		[SerializeField] protected FiringMode _currentFiringMode; //current active firing mode
 		[SerializeField] protected int[] _firingModesAvailable; //available firing modes
 		[SerializeField] protected AAmmo _ammo; //ammunition to fire
@@ -26,29 +25,55 @@ namespace TheLastHope.Weapons
 		protected Timer _delay = new Timer();                       //Timer
 		protected bool _readyToFire = true;                         //Ready to shoot flag
 		protected int _ammoInClip;									//Current ammo in clip
-		protected AudioSource _audioPlayer;
-		#endregion
+        protected int _currentAmmoInClip;
+        protected AudioSource _audioPlayer;
+        #endregion
 
-		#region Abstract Functions
-		public abstract void Fire();                 //Firing (mandatory to implement in inheriting classes)
-		public abstract void Reload(int bulletsInClip);        //Recharging (mandatory to implement in inheriting classes)
+        #region Abstract Functions
+        public abstract void Shot(); //Логика выстрела             
 		public abstract void SwitchFiringMode();
-		#endregion
+        #endregion
+        public void Fire() 
+        //Функция вызывает комманду выстрела проверяя наличие 
+        //патронов и учитывая скорость стрельбы
+        {
+            Debug.Log("Fire!");
+            if (_delay._elapsed == -1)
+            {
+                _readyToFire = true;
+            }
+            if (_currentAmmoInClip == 0)
+            {
+                _readyToFire = false;
+                _delay.Start(_reloadTime);
+                _currentAmmoInClip = _clipSize;
+            }
+            if (_readyToFire && _ammo)
+            {
+                Shot();
+                var snd = this.GetComponent<AudioSource>();
+                snd.Play();
+                _delay.Start(_rateOfFire);
+                _readyToFire = false;
+                _currentAmmoInClip -= 1;
+            }
+                
 
-		public AudioSource AudioPlayer
+        }
+        public AudioSource AudioPlayer
 		{
 			get { return _audioPlayer; }
 			set { _audioPlayer = value; }
 		}
-
+        protected virtual void Start()
+        {
+            _currentAmmoInClip =  _clipSize;
+            
+        }
 		protected virtual void Update()
 		{
-				_delay.Update();
-				if (_delay.IsEvent())
-
-				{
-					_readyToFire = true;
-				}
-		}
+			_delay.Update();
+            
+        }
 	}
 }
