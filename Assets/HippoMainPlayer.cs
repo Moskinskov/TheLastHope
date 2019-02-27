db9db8cfbe19c474a069d1ff819af79b6ca2f8e9
Dmitri Kuzmin <dkuzmin@brite-apps.com>
2019-02-27 04:34:31 +0300

    Trying to implement basic Player death.

---
+++ Assets/HippoMainPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TheLastHope.Data;
+
+namespace TheLastHope.Hippo
+{
+
+	public class HippoMainPlayer: MonoBehaviour
+	{
+		[SerializeField]
+		private float _health = 100.0f;
+
+		public void SetDamage(float damage)
+		{
+			_health -= damage;
+		}
+
+		public void Update() //Temp
+		{
+			if (_health < 1)
+			{
+				Debug.Log("Player wasted!");
+			}
+		}
+
+	}
+}
