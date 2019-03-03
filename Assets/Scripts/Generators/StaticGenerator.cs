f6e702258faaa46d103306ed88c104f1884db42e
DannyTrit <danya17k@gmail.com>
2019-03-03 15:42:31 +0300

    Now static generator create objects by pattern P.S. There's not pattern yet

--- Assets/Scripts/Generators/StaticGenerator.cs
+++ Assets/Scripts/Generators/StaticGenerator.cs
@@ -1,77 +1,116 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System;
+using UnityEngine;
 using TheLastHope.Management.Data;
 using TheLastHope.Management.AbstractLayer;
-using UnityEngine;
 
 namespace TheLastHope.Generators
 {
 	class StaticGenerator : AGenerator
-	{
-		private int startGenerationPosition;
+    {
+        //Pattern structure 5 rows 9 columns, each cell 10x10
 
-		[SerializeField] private GameObject[] prop;
-		[SerializeField] private int step = 1;
-		[SerializeField] private Vector2 activeTerrainGeometry = new Vector2(15, 5); //Active spawn zone x[-15;15] z[-5;5]
-		[SerializeField] private float railsWidth = 2;
+        private int startGenerationPosition;
+        private Vector3[] positions;                 //local temporary pattern 
+        private int activeColumn;
+
 		#region Debugging
-		//void Start()
-		//{
-		//    SceneData sceneData = new SceneData();
-		//    sceneData.rails.Add(new GameObject());
-		//    sceneData.rails[0].transform.position = Vector3.zero;
-		//    Generate(sceneData);
-		//}
-		#endregion
+		//[SerializeField] private GameObject[] prop;
+		//[SerializeField] private int step = 1;
+		//[SerializeField] private Vector2 activeTerrainGeometry = new Vector2(15, 5); //Active spawn zone x[-15;15] z[-5;5]
+                                                                                     //void Start()
+                                                                                     //{
+                                                                                     //    SceneData sceneData = new SceneData();
+                                                                                     //    sceneData.rails.Add(new GameObject());
+                                                                                     //    sceneData.rails[0].transform.position = Vector3.zero;
+                                                                                     //    Generate(sceneData);
+                                                                                     //}
+        #endregion
 
-		public override void Generate(SceneData sceneData)
+        #region Out of use
+        /// <summary>
+        /// Obsolete method
+        /// </summary>
+        /// <param name="sceneData"></param>
+        public override void Generate(SceneData sceneData)
 		{
-			if (sceneData.Props.Count != 0)
+            /*
+			if (sceneData.TargetPropsCount > sceneData.Props.Count)
 			{
-				startGenerationPosition = (int)activeTerrainGeometry.x - 1;
-			}
-			else
-			{
-				startGenerationPosition = (int)-activeTerrainGeometry.x;
-			}
+				startGenerationPosition = (int)activeTerrainGeometry.x;
 
-			for (int i = startGenerationPosition; i < activeTerrainGeometry.x; i += step)
-			{
-				if (sceneData.Props.Count < sceneData.TargetPropsCount)
+				var stepPos = startGenerationPosition;
+				for (var i = sceneData.Props.Count; i < sceneData.TargetPropsCount; i++)
 				{
 					float zCoordinate = UnityEngine.Random.Range(-activeTerrainGeometry.y, activeTerrainGeometry.y);
 
-					if (zCoordinate < sceneData.Rails[sceneData.Rails.Count - 1].transform.position.z + railsWidth &&
+					//if props cross the rails; rails' width is [-5;5]
+					if (zCoordinate < sceneData.Rails[sceneData.Rails.Count - 1].transform.position.z + 5 &&
 						zCoordinate > sceneData.Rails[sceneData.Rails.Count - 1].transform.position.z)
 					{
-						zCoordinate += railsWidth;
+						zCoordinate += 5;
 					}
-					else if (zCoordinate > sceneData.Rails[sceneData.Rails.Count - 1].transform.position.z - railsWidth &&
+					else if (zCoordinate > sceneData.Rails[sceneData.Rails.Count - 1].transform.position.z - 5 &&
 							 zCoordinate < sceneData.Rails[sceneData.Rails.Count - 1].transform.position.z)
 					{
-						zCoordinate -= railsWidth;
+						zCoordinate -= 5;
 					}
 
 					sceneData.Props.Add(Instantiate(
 						prop[UnityEngine.Random.Range(0, prop.Length)],
-						new Vector3(i, 0, zCoordinate),
+						new Vector3(stepPos, 0, zCoordinate),
 						Quaternion.identity));
-				}
-				else
-				{
-					break;
+					stepPos += step;
 				}
 			}
+            */
 		}
 
-		public override void Generate(GameObject obj, SceneData sceneData, List<Vector3> positions)
+		public override void Generate(GameObject gameObject, SceneData sceneData, List<Vector3> positions)
 		{
 			throw new System.NotImplementedException();
 		}
+        #endregion
 
+        private void Initialization()
+        {
+            activeColumn = 0;
+
+            positions = new Vector3[5];                                 //5 rows z={20, 10, 0, -10, -20}; 0 - rails 
+            positions[0] = new Vector3(40, 0, 20);
+            positions[1] = new Vector3(40, 0, 10);
+            positions[2] = new Vector3(40, 0, 0);
+            positions[3] = new Vector3(40, 0, -10);
+            positions[4] = new Vector3(40, 0, -20);
+        }
+
+        /// <summary>
+        /// Generate objects by pattern positions
+        /// </summary>
+        /// <param name="objects">ObjectPool with object's position pattern</param>
+        /// <param name="sceneData"></param>
 		public override void Generate(GameObject[] objects, SceneData sceneData)
-		{
-			throw new NotImplementedException();
+        {
+            if (positions == null)                                      //temporary data
+            {
+                Initialization();
+            }
+
+            if (sceneData.Props[sceneData.Props.Count - 1].transform.position.x < positions[0].x - 10)
+            {
+                for (int i = 0 + (5 * activeColumn); i < 5 + (5 * activeColumn); i++)
+                {
+                    if (objects[i] != null)
+                    {
+                        sceneData.Props.Add(Instantiate(objects[i], positions[i % 5], Quaternion.identity));
+                    }
+                }
+                activeColumn++;
+            }
+            if (activeColumn == 10)
+            {
+                activeColumn = 0;
+            }
 		}
 	}
 }
