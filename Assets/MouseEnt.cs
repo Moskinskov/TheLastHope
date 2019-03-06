b9c9c87ecab7162bf7ef65e35c1cf69734991a30
HastlerN <cool.alexander99@yandex.ru>
2019-03-06 04:36:39 +0300

    UI V1.0

---
+++ Assets/MouseEnt.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseEnt : MonoBehaviour
+{
+    public GameObject uiobj;
+     void OnMouseEnter()
+    {
+        uiobj.SetActive(true);
+    }
+     void OnMouseExit()
+    {
+        uiobj.SetActive(false);
+    }
+}
