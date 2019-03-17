90ac82334f4d800251e05e58cc7f2adb9d25c8fe
Moskinskov <moskinskov@gmail.com>
2019-03-17 16:01:15 +0300

    LaserGun и EnergyGun, производные от AEnergeticWeapon

---
+++ Assets/Scripts/Tests/CanvasInventoryElement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace MosMos
+{
+    public class CanvasInventoryElement : MonoBehaviour, IDragHandler
+    {
+        private InventoryElement _inventoryElement;
+        private Vector3 _mousePosition;
+
+        public InventoryElement Element
+        {
+            get { return _inventoryElement; }
+            set { _inventoryElement = value; }
+        }
+
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            transform.SetParent(default);
+            _mousePosition = Input.mousePosition;
+
+            var ray = Camera.main.ScreenPointToRay(_mousePosition);
+
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                Debug.Log(hit.transform.name);
+
+                Element.PropGO.SetActive(true);
+                Element.PropGO.transform.localPosition = hit.point;
+                Element.PropGO.transform.position.Set(Element.PropGO.transform.localPosition.x, 0.5f, Element.PropGO.transform.localPosition.z);
+            }
+
+
+        }
+    }
+}
---
+++ Assets/Scripts/Tests/DragPanel.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace MosMos
+{
+    public class DragPanel : MonoBehaviour
+    {
+        [SerializeField] private Inventory _inventory;
+        [SerializeField] private Transform _contentPanel;
+        [SerializeField] private CanvasInventoryElement _exampleElement;
+
+        [SerializeField] private Transform _targetTransform;
+        
+        private void Awake()
+        {
+            if (_inventory == null)
+                return;
+
+            foreach (var element in _inventory.PropInventory)
+            {
+                ApplyContent(element);
+            }
+        }
+
+        public void ApplyContent(InventoryElement element)
+        {
+            if (_inventory == null)
+                return;
+            if (!_inventory.PropInventory.Contains(element))
+                _inventory.AddToInventary(element);
+
+            element.PropCanvasElement = Instantiate(_exampleElement, _contentPanel);
+            element.PropCanvasElement.Element = element;
+            element.PropCanvasElement.GetComponent<Image>().sprite = element.Icon;
+        }
+
+        public void RemoveContent(InventoryElement element)
+        {
+            if (_inventory == null)
+                return;
+
+            _inventory.RemoveFromInventory(element);
+            Destroy(element.PropCanvasElement);
+        }
+    }
+}
+
+
