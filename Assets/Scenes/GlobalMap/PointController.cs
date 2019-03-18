05785ebad075c38946226976e260bfd3a153a54c
Buffer112 <gogi-pankraxin@mail.ru>
2019-03-18 21:18:18 +0300

    Моя ветка для создания карты

---
+++ Assets/Scenes/GlobalMap/PointController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointController : MonoBehaviour
+{
+    [SerializeField] public GameObject Arrow = null;
+    [SerializeField] public List<GameObject> Neighboring;
+    [SerializeField] public List<float> Distance;
+    [SerializeField] public string name;
+    [SerializeField] private bool curActive = false;
+    [SerializeField] private bool canActive = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
