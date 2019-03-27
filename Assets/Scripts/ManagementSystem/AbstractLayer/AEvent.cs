30239750c58344f18ecdd171628f9838044a6e6a
Buffer112 <gogi-pankraxin@mail.ru>
2019-03-27 18:54:17 +0300

    Add EventManager in ManagerSystem and AEvent in AbstractLayer.

---
+++ Assets/Scripts/ManagementSystem/AbstractLayer/AEvent.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AEvent : MonoBehaviour
+{
+    [SerializeField] public int count; //Количество вызовов ивента
+    [SerializeField] public bool inf = false; // Бесконечное выполнение ивента - true, конечное - false
+    [SerializeField] public bool call = false; // Позволяет отслеживать что ивент был вызван
+    public abstract bool Condition(); //Условие при котором ивент сработает
+    public abstract void EventCode(); //Что должно произойти при сработке ивента
+    public void ActivateEvent()
+    {
+        EventCode();
+        call = !call;
+        if (!inf)
+        {
+            count--;
+        }
+    }
+    public void Destroyer()
+    {
+        Destroy(this.gameObject);
+    }
+}
