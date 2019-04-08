fbffaadd5f2f94fb5ef36929d001b1366d6b9fba
Buffer112 <gogi-pankraxin@mail.ru>
2019-04-09 02:41:40 +0300

    Add menu and SaveTrain logic

---
+++ Assets/Scenes/Menu/UiMenuController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiMenuController : MonoBehaviour
+{
+    public void Continue()
+    {
+
+    }
+
+    public void NewGame()
+    {
+
+
+    }
+
+    public void Setting()
+    {
+
+    }
+
+
+    public void Exit()
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
