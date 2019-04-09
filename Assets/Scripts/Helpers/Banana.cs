379f113345c3cc1c1ab96afb46ba22e621cc8dcb
Dmitri Kuzmin <dkuzmin@brite-apps.com>
2019-04-09 13:50:45 +0300

    Hangar visual changes.

---
+++ Assets/Scripts/Helpers/Banana.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TheLastHope.Management.AbstractLayer;
+
+namespace TheLastHope.Helpers
+{
+	public class Banana : SaveableBanana
+	{
+		private ABaseObject baseObject;
+
+		void Init()
+		{
+			baseObject = GetComponent<ABaseObject>();
+		}
+
+		public override void Save(int id)
+		{
+			base.Save(id);
+		}
+
+		public override void Load(string[] values)
+		{
+			base.Load(values);
+		}
+	}
+}
---
+++ Assets/Scripts/Helpers/SaveableBanana.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TheLastHope.Management.Data;
+
+namespace TheLastHope.Helpers
+{
+	public class SaveableBanana : MonoBehaviour
+	{
+		protected string banana;
+		private ObjType bananaType;
+
+		private void Init()
+		{
+
+		}
+
+		public virtual void Save(int id)
+		{
+
+		}
+
+		public virtual void Load(string[] values)
+		{
+
+		}
+
+		public void DestroySaveble()
+		{
+
+		}
+	}
+}
