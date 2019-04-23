12f3d152f16f4bc9b49cbf113b6f2fe7774b87e8
Moskinskov <moskinskov@gmail.com>
2019-04-23 19:40:55 +0300

    111

---
+++ Assets/Scripts/GameElements/Weapons/EnemyWeapon.cs
@@ -0,0 +1,49 @@
+﻿using TheLastHope.Management.AbstractLayer;
+using TheLastHope.Management.Data;
+using UnityEngine;
+
+namespace TheLastHope.Weapons
+{
+    public class EnemyWeapon : ARangedWeapon
+    {
+        public virtual void Fire(SceneData sceneData)
+        {
+
+
+            if (WeaponState != WeaponState.ReadyToFire)
+                return;
+
+            weaponState = WeaponState.Firing;
+
+            if (Physics.Raycast(muzzle.position, muzzle.forward, out RaycastHit hit))
+            {
+                if (hit.distance <= MaxRange)
+                {
+                    if (hit.transform.GetComponent<ATurret>())
+                    {
+                        WeaponMethod(hit);
+                    }
+                }
+            }
+        }
+
+        protected override void WeaponMethod(RaycastHit hit)
+        {
+            currentAmmoInClip -= ammoPerShot;
+
+            damageEffect.transform.SetPositionAndRotation(hit.transform.position, Quaternion.Euler(hit.normal));
+            hit.transform.GetComponent<ATurret>().SetDamage(damage);
+            delay.Start(rateOfFire);
+            Effects();
+        }
+
+        protected override void Checks()
+        {
+            base.Checks();
+            if (WeaponState == WeaponState.Empty)
+            {
+                currentAmmoInClip = 100;
+            }
+        }
+    }
+}
--- Assets/Scripts/ManagementSystem/GameManager.cs
+++ Assets/Scripts/ManagementSystem/GameManager.cs
@@ -48,7 +48,7 @@ namespace TheLastHope.Management
         // Start is called before the first frame update
         void Start()
         {
-            SaveLoadManager.Load(out playerTrain, out player);
+            //SaveLoadManager.Load(out playerTrain, out player);
             sceneData = new SceneData();
             //
             playerTrain = FindObjectOfType<TrainManager>().gameObject;
